028c571e2757c8691d487fdc88ca4a9212da0430
LuisIngrassia <ingrassialuispedro@gmail.com>
2025-03-27 12:56:49 -0300

    add services

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,7 +8,7 @@ export function Nav() {
     {
       id: 2,
       name: "Servicios",
-      href: "#",
+      href: "#Services",
     },
     {
       id: 3,
@@ -29,7 +29,7 @@ export function Nav() {
 
   return (
     <header className="fixed top-0 left-0 z-10 flex h-16 w-full items-center justify-between px-6">
-      <p>PUTO</p>
+      <p>LOGO</p>
       <nav className="z-10 h-full w-full max-w-2xl">
         <ul className="flex h-full w-full items-center justify-between">
           {navItems?.map((navItem) => (
